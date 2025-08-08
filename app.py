9af6591a776a1bb1193f6af8eaba71c5cf83ce9a
LEE JUNSEO <yooasiasia0917@gmail.com>
2025-08-09 01:04:01 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+from collections import defaultdict
+
+app = Flask(__name__)
+subjects = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global subjects
+    schedule = {}
+
+    if request.method == 'POST':
+        subject = request.form.get('subject')
+        time = request.form.get('time')
+        action = request.form.get('action')
+
+        if subject and time:
+            try:
+                time = float(time)
+                subjects = [(s, t) for s, t in subjects if s != subject]
+                subjects.append((subject, time))
+            except ValueError:
+                pass
+
+        if action == 'generate':
+            schedule = generate_schedule(subjects)
+
+    return render_template('index.html', subjects=subjects, schedule=schedule)
+
+def generate_schedule(subjects):
+    total_hours = sum(t for _, t in subjects)
+    day_names = ['월', '화', '수', '목', '금', '토', '일']
+    weights = [0.18, 0.18, 0.18, 0.16, 0.10, 0.10, 0.10]
+    daily_targets = [round(total_hours * w, 2) for w in weights]
+    daily_remaining = daily_targets[:]
+    subject_hours = {name: time for name, time in subjects}
+    plan = defaultdict(list)
+
+    index = 0
+    while any(v > 0 for v in subject_hours.values()):
+        name, remaining = list(subject_hours.items())[index % len(subject_hours)]
+        if remaining <= 0:
+            index += 1
+            continue
+        for i in range(7):
+            if subject_hours[name] <= 0 or daily_remaining[i] <= 0:
+                continue
+            alloc = min(subject_hours[name], daily_remaining[i], 2.0)
+            plan[day_names[i]].append((name, alloc))
+            subject_hours[name] -= alloc
+            daily_remaining[i] -= alloc
+        index += 1
+
+    return dict(plan)
+
+if __name__ == '__main__':
+    app.run(debug=True)
